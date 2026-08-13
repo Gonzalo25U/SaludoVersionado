9478a888b272814386d6c0fd74e2309690654b82
Gonzalo25U <gonzalo.s.ulloa@gmail.com>
2026-08-12 23:34:58 -0400

    Version 1.1.0: Creacion de un nuevo endpoint

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,2 @@
 V1.0.0 Creacion del Microservicio
+V1.1.0 Creacion del Endpoint nuevo
--- src/main/java/com/example/demo/controller/SaludoControllerV1.java
+++ src/main/java/com/example/demo/controller/SaludoControllerV1.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,10 @@ public class SaludoControllerV1 {
     @GetMapping("/saludo")
     public String saludo() {
         return "Hola desde la versión 1.0.0 de la API";
+    }
+        @PostMapping
+    public String despedida(){
+        return "despedida de la verision actual (1.1.0)";
     }
     
 }
