c637438a4977a868a37b434e08f4e27794b57761
Gonzalo25U <gonzalo.s.ulloa@gmail.com>
2026-08-19 23:13:47 -0400

    Version 2.1.0: Mejoras en los Endpoints actuales

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@ V1.1.0 Creacion del Endpoint nuevo
 V1.1.1 Correccion de bugs en el endpoint
 V1.2.0 Creacion del nuevo archivo dockerfile
 V2.0.0 Creacion de nuevas funciones de seguridad
+V2.1.0 Mejoras en los Endpoints actuales
--- src/main/java/com/example/demo/controller/SaludoControllerV1.java
+++ src/main/java/com/example/demo/controller/SaludoControllerV1.java
@@ -1,20 +1,28 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/api/v1")
 public class SaludoControllerV1 {
-    @GetMapping("/saludo")
-    public String saludo() {
-        return "Hola desde la versión 1.1.1 de la API, ahora libre de bugs";
+    @GetMapping
+    public String holaMundo(@AuthenticationPrincipal Jwt jwt) {
+        String usuario = jwt.getSubject();
+        return "hola mundo v1.0.0- bug corregido v1.1.1 "+usuario;
     }
-        @PostMapping
-    public String despedida(){
-        return "despedida de la verision actual (1.1.0)";
+    @PostMapping
+    public String despedida(@AuthenticationPrincipal Jwt jwt) {
+        String usuario = jwt.getSubject();
+        return "despedida V1.1.0 "+usuario;
     }
-    
+
+    @GetMapping("/public")
+    public String endpointLibre() {
+        return "enspoint sin validacion";
+    }
 }
